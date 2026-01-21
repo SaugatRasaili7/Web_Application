ce8194d7a745b0d586a7fe5e375ca16782242fe8
Saugat Rasaili <saugatrasaili76@gmail.com>
2026-01-21 18:06:30 +0545

    Refactor Register component form fields

--- vite-project/src/components/Register/Register.jsx
+++ vite-project/src/components/Register/Register.jsx
@@ -3,19 +3,40 @@ function Register(){
         <div>
             <h1> Register</h1>
             <form action="">
-                <label htmlFor="name"> Email :</label>
-                <input type="text" value={"Enter your Email"}/> <br />
+                <label htmlFor="name"> Name :</label>
+                <input
+          type="text"
+          name="name"
+          placeholder="Your Name"
+          value={formData.name}
+          required
+          className="w-full p-2 mb-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+                
+                <label htmlFor="Email"> Email :</label>
+               <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+          className="w-full p-2 mb-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
 
-                <label htmlFor="password"> Password :</label>
-                <input type="password" value={"Enter your password"}/> <br /> 
 
                 <label htmlFor="DOB"> DOB :</label>
-                <input type="date " /> <br />
+                <input
+    type="date"
+    name="dob"
+    value={formData.dob}
+    onChange={handleChange}
+    required
+    className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+  />
 
 
-                <label htmlFor="Age"> Age :</label>
-                <input type="number" value={"Enter your age"} />
-                
+              
 
                 
             </form>
