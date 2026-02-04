066d9242af8e507895ba6fc8f71a3520af6e361e
Saugat Rasaili <saugatrasaili76@gmail.com>
2026-02-04 16:36:12 +0545

    react

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,12 +2,7 @@ import Register from "./components/Register/Register";
 function App(){
   return(
     <div>
- <p> hello guys</p>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
 
-<h2 class = "font-black"> Saugat Here  </h2>
     <Register/>
 
     </div>
---
+++ vite-project/src/components/Input/input.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+export default function input(){
+    return(
+        <input
+          type="text"
+          placeholder="Input"
+          required
+          className="w-full p-2 border rounded"
+        />
+    )
+}
